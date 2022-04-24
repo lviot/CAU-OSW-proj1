d466190cae9377e7f85d945433dd899ef0ae0a28
Louis Viot <louis.viot@epitech.eu>
2022-04-24 18:16:46 +0900

    feat(types): add app global types

---
+++ src/@types/index.d.ts
@@ -0,0 +1,28 @@
+/*
+ * As enum can be used both as type and values,
+ * we need to declare type and export values
+ */
+export enum Direction {
+  TOP = 38, // arrow up
+  RIGHT = 39, // arrow right
+  BOTTOM = 40, // arrow down
+  LEFT = 37, // arrow left
+}
+
+declare global {
+  type Thunk<T> = T | Promise<T>;
+
+  interface Coordinates2D {
+    x: number,
+    y: number,
+  }
+
+  enum Direction {
+    TOP,
+    RIGHT,
+    BOTTOM,
+    LEFT,
+  }
+}
+
+export {};
