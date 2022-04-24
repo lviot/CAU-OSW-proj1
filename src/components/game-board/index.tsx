4956da51ae2a66dee615f3b31dc43b3dd37939e5
Thibault Moynat <moynat.thibault@epitech.eu>
2022-04-24 18:19:48 +0900

    feat(components/game-board): add bare minimal component

---
+++ src/components/game-board/index.tsx
@@ -0,0 +1,16 @@
+import './index.css';
+
+import type { FC } from 'react';
+
+import React, { useEffect, useCallback } from 'react';
+
+const GameBoard: FC = () => {
+  return (
+    <div className="Container">
+      <div className="GameBoard">
+      </div>
+    </div>
+  );
+};
+
+export default GameBoard;
