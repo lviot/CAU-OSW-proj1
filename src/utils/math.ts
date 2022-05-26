f2a2a591853fb1d4c6261219eb1d4844f05f1ea6
Louis Viot <louis.viot@epitech.eu>
2022-05-26 18:38:40 +0900

    refactor(utils/math): make randomCoordinatesExceptValues handling different x and y max values

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,10 +1,17 @@
-export function randomCoordinatesExceptValues(min: number, max: number, invalidValues: Coordinates2D[]): Coordinates2D {
+export interface RandomCoordinatesArgs {
+  x: { min?: number, max: number },
+  y: { min?: number, max: number },
+  invalidValues: Coordinates2D[],
+}
+
+export function randomCoordinatesExceptValues(args: RandomCoordinatesArgs): Coordinates2D {
+  const { x, y, invalidValues } = args;
   let random: Coordinates2D;
 
   do {
     random = {
-      x: Math.floor(Math.random() * (max - min + 1)),
-      y: Math.floor(Math.random() * (max - min + 1)),
+      x: Math.floor(Math.random() * (x.max - (x.min ?? 0) + 1)),
+      y: Math.floor(Math.random() * (y.max - (y.min ?? 0) + 1)),
     };
   } while (invalidValues.some(({ x, y }) => x === random.x && y === random.y));
 
