75fc2e3b6c18b85eba1f4dd1851cd9991de67854
Louis Viot <louis.viot@epitech.eu>
2022-05-26 18:42:59 +0900

    feat(components/game-board): implement player2 input handling

--- src/components/game-board/index.tsx
+++ src/components/game-board/index.tsx
@@ -9,14 +9,18 @@ import { coordinatesToViewPosition } from '@utils/translate';
 import { Direction } from '@app/@types/index.d';
 import { MainMenu, PauseMenu, GameOverMenu } from '@components/menus';
 import RankingMenu from '@components/menus/ranking';
+import {DirectionP2} from '@app/utils/direction';
 
 const GridHeight = 600;
 const BlockSize = GridHeight / GameBoardStore.RowBlocksCount;
 const PauseKey = 27; // ESC
 
 const GameBoard: FC = observer(() => {
-  const eventListener = useCallback(({ keyCode, ...e }: KeyboardEvent) => {
-    if (!store.isAI && Object.values(Direction).includes(keyCode)) store.setDirection(keyCode);
+  const eventListener = useCallback(({ keyCode }: KeyboardEvent) => {
+    if (!store.isAI && store.gameMode === GameMode.DualPlayer && Object.values(DirectionP2).includes(keyCode))
+      store.setDirectionP2(keyCode);
+    if (!store.isAI && Object.values(Direction).includes(keyCode))
+      store.setDirection(keyCode);
     else if (keyCode === PauseKey) store.togglePause();
   }, [store.isAI]);
 
